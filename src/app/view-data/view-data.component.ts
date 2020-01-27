0bb53d1a8681a2a264a66860761c9bc9c1119f5a
somitraj <ranjitkarsomit@gmail.com>
2020-01-27 15:17:20 +0100

    Add files via upload

--- src/app/view-data/view-data.component.ts
+++ src/app/view-data/view-data.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service'; 
 import { HttpResponse } from '@angular/common/http';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { EditDialogueComponent } from '../edit-dialogue/edit-dialogue.component';
 
 
 @Component({
@@ -13,30 +15,61 @@ import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 export class ViewDataComponent implements OnInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;  
-  constructor(private apiService: ApiService) { }
-  
+  constructor(private apiService: ApiService,public dialog: MatDialog) { }
+
   displayedColumns: string[] = ['id', 'username', 'password', 'edit', 'delete'];  
   loginData = new MatTableDataSource();
-
 
   applyFilter(filterValue: string) {
     this.loginData.filter = filterValue.trim().toLowerCase();
   }
 
 	ngOnInit() {
-		this.apiService.get().subscribe(data =>{   
+		this.loadData();
+  }
+
+  public loadData() {
+    this.apiService.get().subscribe((data: any[]) =>{   
       if(!data){
         return;
       }
       // console.log(data)
-      // this.loginData= new MatTableDataSource(data);
+      this.loginData= new MatTableDataSource(data);
       this.loginData.paginator=this.paginator; 
-      this.loginData.sort=this.sort; 
+      this.loginData.sort=this.sort;       
+    })    
+  } 
+  
+  public redirectToUpdate = (i: string, id: string, username: string, password: string) => {     
+    let serverDialogRef = this.dialog.open(EditDialogueComponent, {
+      width: '250px',
+      data: {id: id, username: username, password: password}
+    });
+    // const dialogRef = this.dialog.open(EditDialogueComponent, {
+    //   data: {id: id, username: username, password: password}
+    // });
 
-      
-    })  
-    
-     
-	}
 
+  }
+
+  // dialogRef.afterClosed().subscribe(result => {
+  //   if (result === 1) {
+  //     // When using an edit things are little different, firstly we find record inside DataService by id
+  //     const foundIndex = this.exampleDatabase.dataChange.value.findIndex(x => x.id === this.id);
+  //     // Then you update that record using data from dialogData (values you enetered)
+  //     this.exampleDatabase.dataChange.value[foundIndex] = this.dataService.getDialogData();
+  //     // And lastly refresh table
+  //     this.refreshTable();
+  //   }
+  // }); 
+
+ 
+  public redirectToDelete = (id: string, username: string) => {  
+
+    if(confirm("Are you sure to delete user '" + username + "'")) {
+      this.apiService.deleteData(id).subscribe((data: any[]) => {
+        this.loadData(); 
+      });  
+    }    
+  }
 }
